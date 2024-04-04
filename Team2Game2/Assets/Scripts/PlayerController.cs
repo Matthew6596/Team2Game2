0d743fa8b471382a1dbbf70c5eb203a51ad3508c
rft0202 <rftester@email.neit.edu>
2024-04-04 17:42:27 -0400

    player moves left and right

---
+++ Team2Game2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 10.0f;
+    public float jumpStrength = -10.0f;
+    public float gravity = -9.8f;
+    public float gravityMultiplier = 3.0f;
+    float velocity = 1;
+
+    CharacterController characterController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveZ = Input.GetAxis("Horizontal") * speed;
+        float moveX = Input.GetAxis("Vertical") * speed;
+        Vector3 movement = new Vector3(moveX, 0, moveZ);
+
+        movement = Vector3.ClampMagnitude(movement, speed);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
+
+        velocity += gravity * gravityMultiplier * Time.deltaTime;
+
+        movement.y = velocity;
+
+        movement *= Time.deltaTime;
+        movement = transform.TransformDirection(movement);
+        characterController.Move(movement);
+    }
+
+    public void Jump()
+    {
+        velocity *= jumpStrength;
+    }
+}
