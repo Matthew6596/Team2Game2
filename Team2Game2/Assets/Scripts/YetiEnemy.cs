7898ef88401ab1034ec6835e35dfb872de23f6c1
Matthew6596 <114946013+Matthew6596@users.noreply.github.com>
2024-04-04 17:14:53 -0400

    Improved yeti stuff

--- Team2Game2/Assets/Scripts/YetiEnemy.cs
+++ Team2Game2/Assets/Scripts/YetiEnemy.cs
@@ -7,6 +7,7 @@ public class YetiEnemy : MonoBehaviour
     public GameObject target;
     public float speed;
 
+    public int hunger = 1;
 
     // Start is called before the first frame update
     void Start()
@@ -22,5 +23,26 @@ public class YetiEnemy : MonoBehaviour
         transform.LookAt(target.transform);
         transform.rotation = Quaternion.Euler(0, transform.rotation.eulerAngles.y, 0);
         transform.position = Vector3.MoveTowards(transform.position,target.transform.position,speed*Time.deltaTime);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Chip"))
+        {
+            Destroy(collision.gameObject);
+            hunger--;
+            if (hunger <= 0) //Number of chips needed to sway yeti
+                becomeFriendly();
+        }
+    }
+
+    void becomeFriendly()
+    {
+        //Add friend script to yeti
+        YetiFriend yay = gameObject.AddComponent<YetiFriend>();
+        yay.speed = speed;
+
+        //done, remove enemy script from yeti
+        Destroy(this);
     }
 }
--- Team2Game2/Assets/Scripts/YetiFriend.cs
+++ Team2Game2/Assets/Scripts/YetiFriend.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class YetiFriend : MonoBehaviour
 {
+    public GameObject target;
+    public float speed;
+
     // Start is called before the first frame update
     void Start()
     {
