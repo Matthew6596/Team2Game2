90aa11256dcd8bc089f63f0d0fd0b855844913b7
rft0202 <rftester@email.neit.edu>
2024-04-09 22:08:25 -0400

    Added push animation

--- Team2Game2/Assets/Scripts/BoxScript.cs
+++ Team2Game2/Assets/Scripts/BoxScript.cs
@@ -1,16 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Animations;
 
 public class BoxScript : MonoBehaviour
 {
     public float pushingPower = 1f;
     GameObject box;
 
+    Animator playerAnim;
+
     // Start is called before the first frame update
     void Start()
     {
         box = GameObject.Find("openBox");
+        playerAnim = GameObject.Find("Player").GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -24,10 +28,20 @@ public class BoxScript : MonoBehaviour
         if(other.CompareTag("Player"))
         {
             Debug.Log("Contact!");
+            playerAnim.SetBool("isPushing", true);
             box.transform.position += new Vector3(pushingPower,0,0);
         }
         Debug.Log("bruh");
     }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerAnim.SetBool("isPushing", false);
+        }
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
         Debug.Log("bruh! " + collision.gameObject.name);
